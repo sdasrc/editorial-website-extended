be61de4612b8297b7efbe5329c59bb279dae0306
thephoenix01 <soumyadeep.das.phy14@iitbhu.ac.in>
2020-05-07 12:10:03 +0530

    - Combined sidebar.css, author.css and mystyles.css into main.css - Generated critical css file from https://jonassebastianohlsson.com/criticalpathcssgenerator/, added it to head.html - Added media=print and onload media to other css, following this https://stackoverflow.com/questions/32759272/how-to-load-css-asynchronously

---
+++ assets/images/img-web.sh
@@ -0,0 +1,10 @@
+# Compresses images for web, creates a thumbnail and a webp file.
+echo "Enter the filename (with extension), the new image name and extension - "
+imgfile=$1
+newname=$2
+newimg=$2.$3
+newext=$3
+echo "$imgfile $newimg"
+convert $imgfile -sampling-factor 4:2:0 -strip -quality 85 -interlace Plane -gaussian-blur 0.05 -colorspace RGB $newimg
+convert -resize 5% $newimg $newname-thumb.$newext
+cwebp $newimg -o  $newname.webp
