83b5328c35a28dd798ed88878348ff9ef6395f96
thephoenix01 <soumyadeep.das.phy14@iitbhu.ac.in>
2020-06-04 00:09:51 +0530

    Moved to version 3.9.1. Added author card in posts.

--- assets/images/workshop/img-web.sh
+++ assets/images/workshop/img-web.sh
@@ -1,10 +1,11 @@
 # Compresses images for web, creates a thumbnail and a webp file.
+#  -gaussian-blur 0.05
 echo "Enter the filename (with extension), the new image name and extension - "
 imgfile=$1
 newname=$2
 newimg=$2.$3
 newext=$3
 echo "$imgfile $newimg"
-convert $imgfile -sampling-factor 4:2:0 -strip -quality 85   -gaussian-blur 0.05 -interlace Plane -colorspace RGB $newimg
+convert $imgfile -sampling-factor 4:2:0 -strip -quality 85   -interlace Plane -colorspace RGB $newimg
 convert -resize 5% $newimg $newname-thumb.$newext
 cwebp $newimg -o  $newname.webp
